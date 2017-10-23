e73188acd70833fca23377bc51ad80f206e566bc
smoisset <smoisset@worldremit.com>
2017-10-23 13:57:53 +0100

    Refactor PaymentForm Component

--- src/components/CheckoutArea/PaymentFormComponents.js
+++ src/components/CheckoutArea/PaymentFormComponents.js
@@ -1,26 +1,29 @@
 import React from 'react'
+
 import BasicInput from './BasicInput'
 import ExpiryDate from './ExpiryDate'
 import CheckoutButton from './CheckoutButton'
 
-export const PaymentForm = (props) => {
-    return (
-        <div className="PaymentForm">
-            <form onSubmit={props.onSubmit}>
-                <div className="Title">Payment information</div>
-                <BasicInput
-                    name="name"
-                    label="Name on credit card"
-                    type="text"
-                    placeholder="Sonya Moisset" />
-                <BasicInput
-                    name="card"
-                    label="Credit card number"
-                    type="number"
-                    placeholder="0000 0000 0000 0000" />
-                <ExpiryDate />
-                <CheckoutButton />
-            </form>
-        </div>
-    )
-}
+export const PaymentForm = ({ onSubmit }) => (
+    <div className="PaymentForm">
+        <form onSubmit={ onSubmit }>
+            <div className="Title">Payment information</div>
+
+            <BasicInput
+                name="name"
+                label="Name on credit card"
+                type="text"
+                placeholder="Sonya Moisset" />
+            
+            <BasicInput
+                name="card"
+                label="Credit card number"
+                type="number"
+                placeholder="0000 0000 0000 0000" />
+            
+            <ExpiryDate />
+
+            <CheckoutButton />
+        </form>
+    </div>
+)
