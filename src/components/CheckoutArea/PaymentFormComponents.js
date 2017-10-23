f45b01ab0f1e9612ec728d00a928a2919c1a71b3
smoisset <smoisset@worldremit.com>
2017-10-23 13:54:40 +0100

    Create ExpiryDate Component

--- src/components/CheckoutArea/PaymentFormComponents.js
+++ src/components/CheckoutArea/PaymentFormComponents.js
@@ -1,42 +1,6 @@
 import React from 'react'
 import BasicInput from './BasicInput'
-
-export const ExpiryDate = (props) => {
-    return (
-        <div className="ExpiryDate">
-            <div>
-                <label>Expire on</label>
-                <div className="Expiry">
-                    <select>
-                        <option value="">January</option>
-                        <option value="">February</option>
-                        <option value="">March</option>
-                        <option value="">April</option>
-                        <option value="">May</option>
-                        <option value="">June</option>
-                        <option value="">July</option>
-                        <option value="">August</option>
-                        <option value="">September</option>
-                        <option value="">October</option>
-                        <option value="">November</option>
-                        <option value="">December</option>
-                    </select>
-                    <select name="" id="">
-                        <option value="">2017</option>
-                        <option value="">2018</option>
-                        <option value="">2019</option>
-                        <option value="">2020</option>
-                        <option value="">2021</option>
-                    </select>
-                </div>
-            </div>
-            <div className="CVCField">
-                <label>CVC</label>
-                <input placeholder="000" type="number" />
-            </div>
-        </div>
-    )
-}
+import ExpiryDate from './ExpiryDate'
 
 export const CheckoutButton = (props) => {
     return (
