d096b049a36f24bbac83c9f140382d43a51ed82b
smoisset <smoisset@worldremit.com>
2017-10-23 18:16:10 +0100

    Remove Container Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import Header from './components/Header'
-import Overlay from './components/Overlay'
-import Container from './components/Container'
 import ImagePreview from './components/ImagePreviewArea/ImagePreview'
 import Checkout from './components/CheckoutArea/Checkout'
+
+import { Header, Overlay } from './components'
+
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './App.css';
 
@@ -12,17 +12,12 @@ export default class App extends Component {
     super (props);
 
     this.state = {
-      mounted: false,
       people: 1,
       price: 320.00,
       tax: 20,
       duration: 5,
       discount: 5
     }
-  }
-
-  componentDidMount= () => {
-    this.setState({ mounted: true });
   }
 
   handleSubmit = (event) => {
@@ -35,46 +30,36 @@ export default class App extends Component {
   }
 
   render() {
-    let overlay;
-    let container;
-    
-    if (this.state.mounted) {
-      overlay = (
-        <Overlay image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/jj-2.jpg" />
-      );
-      container = (
-        <Container>
-          <ImagePreview 
-            price={this.state.price}
-            duration={this.state.duration}
-            people={this.state.people}
-            image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/jj-2.jpg"
-          />
-          <Checkout 
-            duration={this.state.duration}
-            discount={this.state.discount}
-            tax={this.state.tax}
-            price={this.state.price}
-            onSubmit={this.handleSubmit}
-          />
-        </Container>
-      );
-    }
-
     return (
       <div className="App">
         <ReactCSSTransitionGroup
           transitionName="overlay"
           transitionEnterTimeout={500}
           transitionLeaveTimeout={300}>
-          {overlay}
+          <div className="Overlay">
+            <Overlay image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/jj-2.jpg" />
+          </div>
         </ReactCSSTransitionGroup>
 
         <ReactCSSTransitionGroup
           transitionName="container"
           transitionEnterTimeout={500}
           transitionLeaveTimeout={300}>
-          {container}
+          <div className="Container">
+            <ImagePreview
+              price={this.state.price}
+              duration={this.state.duration}
+              people={this.state.people}
+              image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/jj-2.jpg"
+            />
+            <Checkout
+              duration={this.state.duration}
+              discount={this.state.discount}
+              tax={this.state.tax}
+              price={this.state.price}
+              onSubmit={this.handleSubmit}
+            />
+          </div>
         </ReactCSSTransitionGroup>
 
         <Header onChange={this.handleChange} />
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Header from './Header'
+import Overlay from './Overlay'
+
+export {
+    Header,
+    Overlay
+}
