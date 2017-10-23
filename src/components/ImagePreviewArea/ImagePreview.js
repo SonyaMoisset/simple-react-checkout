2481a04e0789666786568938575a6872bcd34da3
smoisset <smoisset@worldremit.com>
2017-10-23 14:04:27 +0100

    Refactor ImagePreview Component

--- src/components/ImagePreviewArea/ImagePreview.js
+++ src/components/ImagePreviewArea/ImagePreview.js
@@ -1,20 +1,19 @@
 import React from 'react'
+
 import Meta from './Meta'
 import Information from './Information'
 
-const ImagePreview = (props) => {
-    return (
-        <div className="ImagePreview" style={{'backgroundImage': 'url(' + props.image + ')'}}>
-            <div className="WorkspaceOverview">
-                <Information 
-                    name="Coworking Space, South Korea"
-                    price={props.price}
-                    duration="1" />
-                <Meta
-                    people={props.people} />
-            </div>
+const ImagePreview = ({ image, price, people }) => (
+    <div className="ImagePreview" style={{ 'backgroundImage': 'url(' + image + ')' }}>
+        <div className="WorkspaceOverview">
+            <Information 
+                name="Coworking Space, South Korea"
+                price={price}
+                duration="1" />
+            <Meta
+                people={people} />
         </div>
-    )
-}
+    </div>
+)
 
 export default ImagePreview
